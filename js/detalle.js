59ad1097b4e441b3bdec8990050fc4daab16ccdf
BrianaML <bmikkelsen-loth@udesa.edu.ar>
2024-06-05 15:54:07 -0300

    kinda things

---
+++ js/detalle.js
@@ -0,0 +1,26 @@
+console.log(location);
+
+let queryString= location.search
+
+let queryStringOj= new URLSearchParams(queryString)
+
+let id= queryStringOj.get("id")
+
+console.log(id);
+
+fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(function (res) {
+        return res.json()
+    })
+
+    .then(function (data) {
+        let section= document.querySelector("section")
+        section.innerHTML= `<h2 class="nombre">${data.name}</h2>
+        <img src=${data.image} alt="" class="img"> 
+        <p class="estado">Status:${data.status}</p>
+        <p class="especie">Especie:${data.species}</p> `
+
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
